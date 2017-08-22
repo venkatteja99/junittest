a4691425caf8c17bbc9937535078d6c30d3fe764
venkat.bollineni <venkat.bollineni@CSELAB02-09.bennett.edu.in>
2017-08-22 12:06:55 +0530

    array sorting

--- lab/src/lab/lab_work.java
+++ lab/src/lab/lab_work.java
@@ -1,10 +1,41 @@
 package lab;
 
+
 public class lab_work {
-
+	 static void arraySort(int[] arr) {  
+	        int n = arr.length;  
+	        int temp = 0;  
+	         for(int i=0; i < n; i++){  
+	                 for(int j=1; j < (n-i); j++){  
+	                          if(arr[j-1] > arr[j]){  
+	                                 
+	                                 temp = arr[j-1];  
+	                                 arr[j-1] = arr[j];  
+	                                 arr[j] = temp;
+	                          }
+	                 }
+	                 }
+	                         }  
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		 int arr[] ={2,20,13,3,6,4,9};  
+         
+         System.out.println("Array before sorting");  
+         for(int i=0; i < arr.length; i++){  
+                 System.out.print(arr[i] + " ");  
+         }  
+         System.out.println();  
+           
+         arraySort(arr); 
+          
+         System.out.println("Array After sorting");  
+         for(int i=0; i < arr.length; i++){  
+                 System.out.print(arr[i] + " ");  
+         }  
+		                          
+		                 
+		          
+		  
 	}
 
 }
